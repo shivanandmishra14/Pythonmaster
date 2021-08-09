3ce8f9a5bc3447820a9814bebe87dcb7a42d0047
SHIVANAND <shivanandmishra14@gmail.com>
2021-08-09 21:31:17 +0530

    FIrst

---
+++ operators.py
@@ -0,0 +1,20 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print()
+
+for i in range(1, 4):
+    print(i)
+print()
+
+for i in range(1, a//b):
+    print(i)
+
+for i in range(1, a//b):
+    print(i)
