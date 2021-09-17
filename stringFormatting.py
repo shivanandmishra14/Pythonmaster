9ba60749679bf041cd6a396e49bad3666069c0e9
SHIVANAND <shivanandmishra14@gmail.com>
2021-09-17 11:20:19 +0530

    string formatter

---
+++ stringFormatting.py
@@ -0,0 +1,13 @@
+for i in range (1,13):
+    print("No. squared {0} square {1} cube is {2}".format(i, i**2, i**3))
+
+
+for i in range(1,13):
+    print("No. squared {0:2} square {2:4} cube is {2:4}".format(i, i**2, i**3))
+
+
+#  divide
+for i in range(1,13):
+    print()
+    print("pi is appx {0:12}".format(22/7))
+    print("pi is appx {0:12f}".format(22/7))
