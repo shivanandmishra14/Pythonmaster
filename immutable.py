315196968408a3321427d0327b8e1330a16860cf
SHIVANAND <shivanandmishra14@gmail.com>
2021-12-22 09:47:01 +0530

    mutable and immutable

---
+++ immutable.py
@@ -0,0 +1,14 @@
+# boolean immutable
+result = True
+another_result = result
+print(id(result))
+print(id(another_result))
+
+print()
+
+
+# string immutable
+result = "Correct"
+another_result = result
+print(id(result))
+print(id(another_result))
---
+++ mutable.py
@@ -0,0 +1,7 @@
+computer_parts = ["computer", "CPU", "mouse", "mouse_mat"]
+print(computer_parts)
+
+computer_parts += ["monitor"]
+print(computer_parts)
+
+# now list is getting changed means it is a mutable
