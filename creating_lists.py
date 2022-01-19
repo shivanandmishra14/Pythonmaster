ebde86762be67901e689c843570c0cc5749ec0c7
SHIVANAND <shivanandmishra14@gmail.com>
2022-01-19 08:42:59 +0530

    list

---
+++ creating_lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+even = [1, 3, 5, 7]
+odd = [2, 4, 6, 8]
+
+numbers = even+odd
+print(numbers)
+
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+digits = sorted("23456753")
+print(digits)
