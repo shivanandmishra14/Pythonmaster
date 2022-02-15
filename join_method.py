7b093ba48e094f9a3a4a0107fcb022bb81ac95cb
SHIVANAND <shivanandmishra14@gmail.com>
2022-02-15 08:41:51 +0530

    join method

--- join_method.py
+++ join_method.py
@@ -12,3 +12,10 @@ value = separator.join(alpha)
 print(value)
 
 #  items must be string to join
+
+
+
+# Pallindrome
+# test_num = str(121)
+# verify = test_num == test_num[::-1]
+# print("It is pallindrome")
---
+++ split_menthod.py
@@ -0,0 +1,6 @@
+panagram = "The Dalda is panagram"
+splitted = panagram.split()
+print(splitted)
+
+number = "1,2,7,8,9,0"
+print(number.split(","))
