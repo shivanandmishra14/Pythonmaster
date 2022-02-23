068c08653a1d6b049f9a5e2593dfa5a94c68765c
SHIVANAND <shivanandmishra14@gmail.com>
2022-02-23 08:17:47 +0530

    unpack few more tuple

---
+++ unpack_tuple_1.py
@@ -0,0 +1,6 @@
+# for t in enumerate("abcdefgh"):
+#     print(t)
+
+for t in enumerate("abcdefgh"):
+    index, character = t
+    print(index, character)
---
+++ unpack_tuple_2.py
@@ -0,0 +1,6 @@
+metallica = "ride", "Metallica", 1986
+
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
